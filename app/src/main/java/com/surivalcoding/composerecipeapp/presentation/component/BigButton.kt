af9e848da7746fa8095f9bd873b41ed92b6458bd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-01-14 18:16:40 +0900

    refactor: BigButton에 글자와 아이콘 사이에 11만큼 여백 추가

--- app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/BigButton.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/BigButton.kt
@@ -4,19 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +56,7 @@ fun BigButton(
                     ),
                 )
             }
+            Spacer(modifier = Modifier.width(11.dp))
             Icon(
                 modifier = Modifier.size(20.dp),
                 imageVector = Icons.AutoMirrored.Default.ArrowForward,
