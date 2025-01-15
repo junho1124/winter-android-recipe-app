58af5d50379949a29e33064f2f4d04bd3ed40e5a
junho1124 <wnsghqwe@gmail.com>
2025-01-15 20:30:08 +0900

    feat(presentation): FilterButton 컴포넌트 추가
    설정 가능한 텍스트 및 아이콘 지원을 가진 FilterButton 컴포저블 함수 추가.
    선택 상태 처리 및 선택 상태에 따른 스타일링을 포함합니다.
    
    이슈: master (이슈 번호를 찾을 수 없음)

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/FilterButton.kt
@@ -0,0 +1,87 @@
+package com.surivalcoding.composerecipeapp.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.surivalcoding.composerecipeapp.ui.AppColors
+import com.surivalcoding.composerecipeapp.ui.AppTextStyles
+
+@Composable
+fun FilterButton(modifier: Modifier = Modifier, buttonText: String, icon: ImageVector? = null) {
+    var isSelect by remember { mutableStateOf(false) }
+
+    TextButton(
+        onClick = {
+            isSelect = !isSelect
+        },
+        modifier
+            .padding(vertical = 5.dp, horizontal = 10.dp)
+            .background(
+                color = if (isSelect) {
+                    AppColors.primary
+                } else AppColors.withe,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = AppColors.primary,
+                shape = RoundedCornerShape(10.dp)
+            ),
+    ) {
+        Text(
+            buttonText,
+            modifier = modifier,
+            style = AppTextStyles.kS16LH24.copy(
+                color = if (isSelect) {
+                    AppColors.withe
+                } else AppColors.primary
+            )
+        )
+        if(icon != null) {
+            Box(
+                modifier = Modifier.padding(start = 5.dp)
+            )
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = if (isSelect) {
+                    AppColors.withe
+                } else AppColors.primary,
+                modifier = Modifier.size(18.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FilterButtonPreview() {
+    Column {
+        FilterButton(buttonText = "Filter")
+        Row {
+
+            arrayOf("1", "2", "3", "4", "5").forEach {
+                FilterButton(buttonText = it, icon = Icons.Default.Star)
+            }
+        }
+    }
+}
