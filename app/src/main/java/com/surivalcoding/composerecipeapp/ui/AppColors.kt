6930e70684ff0daa5ae0d7ed58ca5f5db9bb4d59
junho1124 <wnsghqwe@gmail.com>
2025-01-16 16:03:14 +0900

    feat(ui): material-icons-core 및 material-icons-extended를 추가하고 UI 스타일 업데이트(master)

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppColors.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppColors.kt
@@ -3,13 +3,37 @@ package com.surivalcoding.composerecipeapp.ui
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
-    val primary = Color(0xFF129575)
-    val primarySub = Color(0xFF71B1A1)
-    val orange = Color(0xFFFFAD30)
-    val orangeSub = Color(0xFFFFE1B3)
-    val withe = Color(0xFFFFFFFF)
-    val cardBackground = Color(0xFFD9D9D9)
+    // Neutral Colors
+    val black = Color(0xFF000000)
+    val gray1 = Color(0xFF484848)
+    val gray2 = Color(0xFF797979)
+    val gray3 = Color(0xFFA6A6A6)
+    val gray4 = Color(0xFFD6D6D6)
+    val white = Color(0xFFFFFFFF)
 
-    val textGrey = Color(0xFFA9A9A9)
-    val textBlack = Color(0xFF121212)
+    val labelColor = Color(0xFF1F1F1F)
+
+    // Primary Colors
+    val primary100 = Color(0xFF129575)
+    val primary80 = Color(0xFF71B1A1)
+    val primary60 = Color(0xFFAFD3CA)
+    val primary40 = Color(0xFFD6EBE7)
+    val primary20 = Color(0xFFF0FAF9)
+
+    // Secondary Colors
+    val secondary100 = Color(0xFFFFC300)
+    val secondary80 = Color(0xFFFFA51A)
+    val secondary60 = Color(0xFFFFBA4D)
+    val secondary40 = Color(0xFFFFCE80)
+    val secondary20 = Color(0xFFFFE1B3)
+
+    // Rating Color
+    val rating = Color(0xFFFFAD30)
+
+    // Warning Colors
+    val warningDark = Color(0xFFE94A59)
+    val warningLight = Color(0xFFFFE1E7)
+
+    // Success Color
+    val success = Color(0xFF31B057)
 }
--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppTextStyles.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppTextStyles.kt
@@ -1,46 +1,91 @@
 package com.surivalcoding.composerecipeapp.ui
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import com.surivalcoding.composerecipeapp.R
 
 object AppTextStyles {
-    private val poppinsFamily = FontFamily(
-        Font(R.font.poppins_bold, FontWeight.Bold),
-        Font(R.font.poppins_regular)
+    // Poppins - Bold
+    val titleTextBold = TextStyle(
+        fontSize = 50.sp,
+        lineHeight = 75.sp,
+        fontWeight = FontWeight.Bold
     )
-
+    val headerTextBold = TextStyle(
+        fontSize = 30.sp,
+        lineHeight = 45.sp,
+        fontWeight = FontWeight.Bold
+    )
+    val largeTextBold = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
+    val mediumTextBold = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+        fontWeight = FontWeight.Bold
+    )
     val normalTextBold = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.Bold,
-        fontFamily = poppinsFamily,
-    )
-
-    val smallButtonText = normalTextBold.copy(
-        fontSize = 11.sp,
-        lineHeight = 16.5.sp,
-        textAlign = TextAlign.Center,
-    )
-
-    val kS16LH24 = TextStyle(
-        fontSize = 16.sp,
         lineHeight = 24.sp,
-        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Bold
     )
-    val kS14LH21 = TextStyle(
+    val smallTextBold = TextStyle(
         fontSize = 14.sp,
         lineHeight = 21.sp,
-        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Bold
     )
-    val kS8LH12 = TextStyle(
+
+    // Poppins - Regular
+    val titleTextRegular = TextStyle(
+        fontSize = 50.sp,
+        lineHeight = 75.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val headerTextRegular = TextStyle(
+        fontSize = 30.sp,
+        lineHeight = 45.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val largeTextRegular = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val mediumTextRegular = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val normalTextRegular = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val smallTextRegular = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val smallLabelRegular = TextStyle(
         fontSize = 8.sp,
         lineHeight = 12.sp,
-        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal
     )
-
-
+    val smallLabelBold = TextStyle(
+        fontSize = 8.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.Bold
+    )
+    val smallerTextRegular = TextStyle(
+        fontSize = 11.sp,
+        lineHeight = 16.5.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val smallerTextBold = TextStyle(
+        fontSize = 11.sp,
+        lineHeight = 16.5.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
