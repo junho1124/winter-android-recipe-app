a9107488bbf7c054a9f9bc9b71353e7c0194dcfb
junho1124 <wnsghqwe@gmail.com>
2025-01-18 19:24:44 +0900

    feat: Added kotlinx.serialization annotations to RecipeDto classes.

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/dto/RecipeDto.kt
@@ -0,0 +1,33 @@
+package com.surivalcoding.composerecipeapp.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecipeWrapperDto(
+    val recipes: List<RecipeDto>? = null
+)
+
+@Serializable
+data class RecipeDto(
+    val category: String? = null,
+    val id: Int? = null,
+    val name: String? = null,
+    val image: String? = null,
+    val chef: String? = null,
+    val time: String? = null,
+    val rating: Float? = null,
+    val ingredients: List<IngredientWrapperDto>? = null
+)
+
+@Serializable
+data class IngredientWrapperDto(
+    val ingredient: IngredientDto? = null,
+    val amount: Int? = null
+)
+
+@Serializable
+data class IngredientDto(
+    val id: Int? = null,
+    val name: String? = null,
+    val image: String? = null
+)
