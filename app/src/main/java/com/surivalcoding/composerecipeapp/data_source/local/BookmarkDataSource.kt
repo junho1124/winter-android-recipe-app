76870bb15c05053fec1750e1ef4a195bc6df7fe7
junho1124 <wnsghqwe@gmail.com>
2025-01-21 18:16:55 +0900

    fix: BookmarkDataSource 인터페이스에 isRecipeSaved 메서드 추가
    BookmarkDataSource 인터페이스에 isRecipeSaved 메서드를 추가했습니다. 이 메서드는 recipeId를 매개변수로 받아 해당 레시피가 저장되어 있는지 여부를 반환합니다. 해당 변경 사항은 MockBookmarkDataSourceImpl 클래스에서 구현되었습니다. (#1234)

--- app/src/main/java/com/surivalcoding/composerecipeapp/data_source/local/BookmarkDataSource.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data_source/local/BookmarkDataSource.kt
@@ -1,6 +1,7 @@
 package com.surivalcoding.composerecipeapp.data_source.local
 
 interface BookmarkDataSource {
+    fun isRecipeSaved(recipeId: Int): Boolean
     fun getSavedRecipes(): List<Int>
     fun saveRecipe(recipeId: Int)
     fun removeRecipe(recipeId: Int)
--- app/src/main/java/com/surivalcoding/composerecipeapp/data_source/local/MockBookmarkDataSourceImpl.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data_source/local/MockBookmarkDataSourceImpl.kt
@@ -2,6 +2,10 @@ package com.surivalcoding.composerecipeapp.data_source.local
 
 class MockBookmarkDataSourceImpl : BookmarkDataSource {
     private val savedRecipes = mutableListOf<Int>(1, 2, 3)
+    override fun isRecipeSaved(recipeId: Int): Boolean {
+        return savedRecipes.contains(recipeId)
+    }
+
     override fun getSavedRecipes(): List<Int> {
         return savedRecipes
     }
